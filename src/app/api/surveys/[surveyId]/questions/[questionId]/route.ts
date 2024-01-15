df3ad0c944cf3fdd0dabc32cfd195ce5951b032c
SergejBojkovski <sergej.bojkovski@gmail.com>
2024-01-15 19:44:47 +0100

    2

--- src/app/api/surveys/[surveyId]/questions/[questionId]/route.ts
+++ src/app/api/surveys/[surveyId]/questions/[questionId]/route.ts
@@ -24,39 +24,6 @@ export const DELETE = routeHandler(async (request, context) => {
     });
 
     return response;
-  } else if (request.method === "PUT") {
-    // Update logic
-    const body = await request.json();
-    const validation = await Question.safeParseAsync(body);
-
-    if (!validation.success) {
-      throw validation.error;
-    }
-
-    const { data } = validation;
-
-    const updatedSurvey = await prisma.survey.update({
-      where: {
-        id: surveyId,
-      },
-      data: {
-        questions: {
-          update: [
-            {
-              where: {
-                id: questionId,
-              },
-              data,
-            },
-          ],
-        },
-      },
-      include: {
-        questions: true,
-      },
-    });
-
-    return updatedSurvey;
   } else {
     // unsupported methods
     return {
@@ -64,4 +31,72 @@ export const DELETE = routeHandler(async (request, context) => {
       details: `Method ${request.method} is not supported for this route.`,
     };
   }
+});
+
+export const PUT = routeHandler(async (request, context) => {
+  const { surveyId, questionId } = context.params;
+  const body = await request.json();
+  const validation = await Question.safeParseAsync(body);
+
+  if (!validation.success) {
+    throw validation.error;
+  }
+
+  const { data } = validation;
+
+  // Handle position update
+  if (data.position !== undefined) {
+    // Retrieve the existing question
+    const existingQuestion = await prisma.question.findUnique({
+      where: {
+        id: questionId,
+      },
+    });
+
+    if (!existingQuestion) {
+      throw new Error(`Question with id ${questionId} not found.`);
+    }
+
+    // Calculate the difference in positions
+    const positionDifference = data.position - existingQuestion.position;
+
+    // Update the positions of questions
+    await prisma.question.updateMany({
+      where: {
+        surveyId,
+        position: {
+          gte: Math.min(existingQuestion.position, data.position),
+          lte: Math.max(existingQuestion.position, data.position),
+        },
+      },
+      data: {
+        position: {
+          increment: positionDifference,
+        },
+      },
+    });
+  }
+
+  const updatedSurvey = await prisma.survey.update({
+    where: {
+      id: surveyId,
+    },
+    data: {
+      questions: {
+        update: [
+          {
+            where: {
+              id: questionId,
+            },
+            data,
+          },
+        ],
+      },
+    },
+    include: {
+      questions: true,
+    },
+  });
+
+  return updatedSurvey;
 });
